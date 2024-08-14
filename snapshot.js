5900ddd38133529ac43e2b1be477debcff59505f
Kuro Denjiro <kurodenjiro1@gmail.com>
2024-08-15 00:16:31 +0700

    Update snapshot.js

--- snapshot.js
+++ snapshot.js
@@ -26,64 +26,64 @@ async function run() {
     });
     
      const provider = new providers.JsonRpcProvider({ url: "https://rpc.mainnet.near.org" });
-    const accountList = await provider.query({
-        request_type: "call_function",
-        account_id: "registry.potlock.near",
-        method_name: "get_projects",
-        args_base64: (Buffer.from(JSON.stringify({}))).toString("base64"),
-        finality: "optimistic",
-    })
-    const projectObject = (JSON.parse(Buffer.from(accountList.result).toString()));
-    const data = await Object.values(projectObject).map(async (project, index) => {
-        if (project.status === "Approved") {
-            const projectDetail = await provider.query({
-                request_type: "call_function",
-                account_id: "social.near",
-                method_name: "get",
-                args_base64: (Buffer.from(JSON.stringify({ "keys": [`${project.id}/profile/**`] }))).toString("base64"),
-                finality: "optimistic",
-            })
-            const dataProjectJson = (JSON.parse(Buffer.from(projectDetail.result).toString()));
-            const data = Object.keys(dataProjectJson).map((key) => {
-                const data = Object.values(dataProjectJson).map((item) => {
-                    const data = {
-                        index: index,
-                        accountId: project.id == key && key,
-                        projectId: project.id == key && key,
-                        category: item.profile.category?.text ? [item.profile.category.text] : item.profile.category ? [item.profile.category] : JSON.parse(item.profile.plCategories),
-                        backgroundImage: item.profile?.backgroundImage ? `https://ipfs.near.social/ipfs/${item.profile.backgroundImage.ipfs_cid}` : '',
-                        image: item.profile?.image ? `https://ipfs.near.social/ipfs/${item.profile.image.ipfs_cid}` : '',
-                        name: item.profile?.name,
-                        description: item.profile?.description,
-                        tagline: item.profile?.tagline,
-                        socialUrl: item.profile?.linktree,
-                        website: item.profile?.website,
-                        tags: Object.keys(item.profile?.tags || []),
-                        url: `https://app.potlock.org/?tab=project&projectId=${project.id}`
-                    }
-                    return data;
-                })
-                return data[0];
-            })
-            return data[0];
-        }
-        // projectList.push(data)
-    });
-    const projects = await Promise.all(data);
-    const documentsProject = [];
-    for (const projectDetail of projects) {
-        const pageContent = JSON.stringify(projectDetail);
-        if (projectDetail && projectDetail.accountId) {
-            const metadata = {
-                source: `https://app.potlock.org/?tab=project&projectId=${projectDetail.accountId}`,
-                name: projectDetail.name,
-                accountId: projectDetail.accountId,
-            };
-            documentsProject.push(new Document({ pageContent, metadata }));
-        }
-    }
+    // const accountList = await provider.query({
+    //     request_type: "call_function",
+    //     account_id: "registry.potlock.near",
+    //     method_name: "get_projects",
+    //     args_base64: (Buffer.from(JSON.stringify({}))).toString("base64"),
+    //     finality: "optimistic",
+    // })
+    // const projectObject = (JSON.parse(Buffer.from(accountList.result).toString()));
+    // const data = await Object.values(projectObject).map(async (project, index) => {
+    //     if (project.status === "Approved") {
+    //         const projectDetail = await provider.query({
+    //             request_type: "call_function",
+    //             account_id: "social.near",
+    //             method_name: "get",
+    //             args_base64: (Buffer.from(JSON.stringify({ "keys": [`${project.id}/profile/**`] }))).toString("base64"),
+    //             finality: "optimistic",
+    //         })
+    //         const dataProjectJson = (JSON.parse(Buffer.from(projectDetail.result).toString()));
+    //         const data = Object.keys(dataProjectJson).map((key) => {
+    //             const data = Object.values(dataProjectJson).map((item) => {
+    //                 const data = {
+    //                     index: index,
+    //                     accountId: project.id == key && key,
+    //                     projectId: project.id == key && key,
+    //                     category: item.profile.category?.text ? [item.profile.category.text] : item.profile.category ? [item.profile.category] : JSON.parse(item.profile.plCategories),
+    //                     backgroundImage: item.profile?.backgroundImage ? `https://ipfs.near.social/ipfs/${item.profile.backgroundImage.ipfs_cid}` : '',
+    //                     image: item.profile?.image ? `https://ipfs.near.social/ipfs/${item.profile.image.ipfs_cid}` : '',
+    //                     name: item.profile?.name,
+    //                     description: item.profile?.description,
+    //                     tagline: item.profile?.tagline,
+    //                     socialUrl: item.profile?.linktree,
+    //                     website: item.profile?.website,
+    //                     tags: Object.keys(item.profile?.tags || []),
+    //                     url: `https://app.potlock.org/?tab=project&projectId=${project.id}`
+    //                 }
+    //                 return data;
+    //             })
+    //             return data[0];
+    //         })
+    //         return data[0];
+    //     }
+    //     // projectList.push(data)
+    // });
+    // const projects = await Promise.all(data);
+    // const documentsProject = [];
+    // for (const projectDetail of projects) {
+    //     const pageContent = JSON.stringify(projectDetail);
+    //     if (projectDetail && projectDetail.accountId) {
+    //         const metadata = {
+    //             source: `https://app.potlock.org/?tab=project&projectId=${projectDetail.accountId}`,
+    //             name: projectDetail.name,
+    //             accountId: projectDetail.accountId,
+    //         };
+    //         documentsProject.push(new Document({ pageContent, metadata }));
+    //     }
+    // }
     
-    vectorStore.addDocuments(documentsProject)
+    // vectorStore.addDocuments(documentsProject)
     // pots
     const potList = await provider.query({
         request_type: "call_function",
@@ -130,9 +130,11 @@ async function run() {
     });
     const pots = await Promise.all(dataPot);
     const documentsPots = [];
+    
     for (const potDetail of pots) {
         const pageContent = JSON.stringify(potDetail);
-        if (potDetail && potDetail.id) {
+        
+        if (potDetail && potDetail.potId) {
             const metadata = {
                 source: `https://app.potlock.org/?tab=pot&potId=${potDetail.id}`,
             };
